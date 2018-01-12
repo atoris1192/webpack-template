d2e314b862a7033ec1216779016e9ab960716e6c
k.niuishi <atoris1192@gmail.com>
2018-01-12 12:05:23 +0900

    babel環境セット

--- src/sub.js
+++ src/sub.js
@@ -1,4 +1,4 @@
 
 export function hello() {
-  alert('---------------');
+  alert('cdddddddddd')
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,27 @@ module.exports = {
     path: `${__dirname}/build`,
     filename: 'bundle.js'
   },
+  devServer: {
+    contentBase: 'build',
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['env', {'modules': false}]
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
   devtool: 'source-map',
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
